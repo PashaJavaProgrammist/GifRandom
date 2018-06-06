1d384f9df7c0e9461cf8ef626e829d7106cb4238
Pavel <pasha280294@gmail.com>
2018-06-07 02:14:09 +0300

    databinding (in progress)

--- app/src/main/java/com/haretskiy/pavel/gifrandom/BindingAdapters.kt
+++ app/src/main/java/com/haretskiy/pavel/gifrandom/BindingAdapters.kt
@@ -1,26 +1,28 @@
 package com.haretskiy.pavel.gifrandom
 
 import android.databinding.BindingAdapter
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import com.haretskiy.pavel.gifrandom.utils.ImageLoader
 import com.haretskiy.pavel.gifrandom.utils.ImageLoaderImpl
 
 
-@BindingAdapter("imageUrl"/*, "progress"*/, "imageLoader")
-fun ImageView.loadImage(url: String/*, observableProgress: ObservableInt*/, imageLoader: ImageLoader) {
+@BindingAdapter("imageUrl", "progress", "imageLoader")
+fun ImageView.loadImage(url: String, progressBar: ProgressBar, imageLoader: ImageLoader) {
 
     imageLoader.loadImage(this, url, object : ImageLoaderImpl.LoadListener {
 
         override fun onLoadStarted() {
-//             observableProgress.set(View.VISIBLE)
+            progressBar.visibility = View.VISIBLE
         }
 
         override fun onLoadFinished() {
-//             observableProgress.set(View.GONE)
+            progressBar.visibility = View.GONE
         }
 
         override fun onLoadFailed() {
-//             observableProgress.set(View.GONE)
+            progressBar.visibility = View.GONE
         }
     })
 }
--- app/src/main/java/com/haretskiy/pavel/gifrandom/viewModels/GifHolderViewModel.kt
+++ app/src/main/java/com/haretskiy/pavel/gifrandom/viewModels/GifHolderViewModel.kt
@@ -3,15 +3,17 @@ package com.haretskiy.pavel.gifrandom.viewModels
 import android.databinding.ObservableField
 import android.databinding.ObservableInt
 import android.view.View
+import android.widget.ProgressBar
 import com.haretskiy.pavel.gifrandom.EMPTY_STRING
+import com.haretskiy.pavel.gifrandom.databinding.ItemHolderBinding
 import com.haretskiy.pavel.gifrandom.models.Data
 import com.haretskiy.pavel.gifrandom.utils.ImageLoader
 
 
-class GifHolderViewModel(imageLoader: ImageLoader, private val data: Data) {
+class GifHolderViewModel(imageLoader: ImageLoader, private val data: Data, mItemGifBinding: ItemHolderBinding) {
 
     var progress: ObservableInt = ObservableInt(View.VISIBLE)
-    var observableProgressBar = ObservableField<ObservableInt>(progress)
+    var observableProgressBar = ObservableField<ProgressBar>(mItemGifBinding.progressBar)
     var url = ObservableField<String>(data.images?.original?.url ?: EMPTY_STRING)
     var loader = ObservableField<ImageLoader>(imageLoader)
 
