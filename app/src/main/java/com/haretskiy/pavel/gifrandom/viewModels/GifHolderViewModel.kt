5d2f55cef584b9c24474d99b068c7bb421431351
Pavel <pasha280294@gmail.com>
2018-06-07 02:16:02 +0300

    databinding (in progress)

--- app/src/main/java/com/haretskiy/pavel/gifrandom/viewModels/GifHolderViewModel.kt
+++ app/src/main/java/com/haretskiy/pavel/gifrandom/viewModels/GifHolderViewModel.kt
@@ -13,7 +13,7 @@ import com.haretskiy.pavel.gifrandom.utils.ImageLoader
 class GifHolderViewModel(imageLoader: ImageLoader, private val data: Data, mItemGifBinding: ItemHolderBinding) {
 
     var progress: ObservableInt = ObservableInt(View.VISIBLE)
-    var observableProgressBar = ObservableField<ProgressBar>(mItemGifBinding.progressBar)
+    var progressBar = ObservableField<ProgressBar>(mItemGifBinding.progressBar)
     var url = ObservableField<String>(data.images?.original?.url ?: EMPTY_STRING)
     var loader = ObservableField<ImageLoader>(imageLoader)
 
