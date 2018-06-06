9adece629451294f23229035ba10ec2db43a584d
Pavel <pasha280294@gmail.com>
2018-06-07 01:46:51 +0300

    databinding (in progress)

--- app/src/main/java/com/haretskiy/pavel/gifrandom/BindingAdapters.kt
+++ app/src/main/java/com/haretskiy/pavel/gifrandom/BindingAdapters.kt
@@ -1,26 +1,26 @@
 package com.haretskiy.pavel.gifrandom
 
 import android.databinding.BindingAdapter
-import android.util.Log
 import android.widget.ImageView
+import com.haretskiy.pavel.gifrandom.utils.ImageLoader
+import com.haretskiy.pavel.gifrandom.utils.ImageLoaderImpl
 
 
-@BindingAdapter("imageUrl"/*, "progressBar", "imageLoader"*/)
-fun ImageView.loadImage(url: String/*, progress: ObservableInt, imageLoader: ImageLoader*/) {
+@BindingAdapter("imageUrl"/*, "progressBar"*/, "imageLoader")
+fun ImageView.loadImage(url: String/*, progress: ObservableInt*/, imageLoader: ImageLoader) {
 
-    Log.d("BindingAdapter", "BindingAdapter $url")
-    /* imageLoader.loadImage(this, url, object : ImageLoaderImpl.LoadListener {
+    imageLoader.loadImage(this, url, object : ImageLoaderImpl.LoadListener {
 
-         override fun onLoadStarted() {
-             progress.set(View.VISIBLE)
-         }
+        override fun onLoadStarted() {
+//             progress.set(View.VISIBLE)
+        }
 
-         override fun onLoadFinished() {
-             progress.set(View.GONE)
-         }
+        override fun onLoadFinished() {
+//             progress.set(View.GONE)
+        }
 
-         override fun onLoadFailed() {
-             progress.set(View.GONE)
-         }
-     })*/
+        override fun onLoadFailed() {
+//             progress.set(View.GONE)
+        }
+    })
 }
