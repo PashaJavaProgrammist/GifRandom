2ece88a36b2d7f2e45e84d3042f59f245e1d8898
Pavel <pasha280294@gmail.com>
2018-06-10 21:31:44 +0300

    pagging(start)

--- app/src/main/java/com/haretskiy/pavel/gifrandom/data/RepositoryImpl.kt
+++ app/src/main/java/com/haretskiy/pavel/gifrandom/data/RepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.haretskiy.pavel.gifrandom.data
 
 import com.haretskiy.pavel.gifrandom.EMPTY_STRING
+import com.haretskiy.pavel.gifrandom.models.GifResponse
 import com.haretskiy.pavel.gifrandom.rest.RestApiImpl
 import io.reactivex.Observable
 
@@ -8,15 +9,13 @@ class RepositoryImpl(
         private val restApi: RestApiImpl) : Repository {
 
     override fun loadGifs(rating: String, offset: String): Observable<List<String>> =
-            restApi.loadGifs(rating).map {
-                val list = it.data.map {
-                    it.images?.original?.url ?: EMPTY_STRING
-                }
-                list
-            }
+            transformToListString(restApi.loadGifs(rating))
 
     override fun loadGifsByWord(word: String, rating: String, offset: String): Observable<List<String>> =
-            restApi.loadGifsByWord(word, rating).map {
+            transformToListString(restApi.loadGifsByWord(word, rating))
+
+    private fun transformToListString(response: Observable<GifResponse>): Observable<List<String>> =
+            response.map {
                 val list = it.data.map {
                     it.images?.original?.url ?: EMPTY_STRING
                 }
